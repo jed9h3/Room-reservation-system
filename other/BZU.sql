2cb13472694820102b3366b973d2c2e8e1b4a8d9
Majed Alghoul <menta8seca@gmail.com>
2023-07-14 18:30:04 +0300

    sample database modified

--- other/BZU.sql
+++ other/BZU.sql
@@ -4,7 +4,6 @@ create table Floors(Fid integer primary key);
 create table Apartments(Aid integer primary key,Fid integer,foreign key(Fid) references Floors (Fid));
 create table Rooms(Rid integer primary key,Rstatus integer,Rprice integer,Rtype integer,Aid integer, foreign key(Aid) references Apartments(Aid));
 create table Students(Sid integer primary key,Sname varchar(50),Sdob date);
-drop table Employees_phones;
 create table Students_phones(Sid integer,Sphone bigint,primary key (Sid,Sphone),foreign key (Sid) references Students(Sid) ON DELETE CASCADE  );
 create table Employees(Eid integer primary key,Ename varchar(50),Edob date);
 create table Employees_phones(Eid integer,Ephone bigint,primary key (Eid,Ephone),foreign key (Eid) references Employees(Eid) ON DELETE CASCADE  );
@@ -28,9 +27,6 @@ insert into Rooms value(7000,0,600,0,400);
 insert into Rooms value(8000,0,600,0,400);
 insert into Rooms value(9000,0,600,0,500);
 insert into Rooms value(10000,0,600,0,500);
-drop table payments;
-drop table rent;
-drop table Rent;
 insert into Employees value(20,'khalid','1990-05-09');
 insert into Employees value(20,'Amier','1989-11-08');
 
